d30e3383d1d2cfca72718c546a501fe3bfd8c246
Reshad007 <reshad_aziz@yahoo.com>
2019-05-26 00:44:15 -0700

    Added Redux

--- src/BudgetingApp.js
+++ src/BudgetingApp.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import Table from "./components/Table";
 
 function BudgetingApp() {
   return (
-    <div className="container">
-      <h1 className="text-center my-4">Budgeting App</h1>
-      <Table />
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <h1 className="text-center my-4">Budgeting App</h1>
+        <Table />
+      </div>
+    </Provider>
   );
 }
 

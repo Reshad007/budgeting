5623371c0d23e09ea677321d25bfa2ae8f3c87ed
Reshad007 <reshad_aziz@yahoo.com>
2019-05-25 23:17:35 -0700

    Initial commit

---
+++ src/BudgetingApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BudgetingApp() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Budgeting App</h1>
+    </div>
+  );
+}
+
+export default BudgetingApp;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import BudgetingAppApp from "./BudgetingApp";
+
+ReactDOM.render(<BudgetingAppApp />, document.getElementById("root"));
